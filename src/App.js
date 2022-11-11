955d10cb0e973635413d66f0c0ee48c9043017c3
Anish Agrawal <agrawalanish42@gmail.com>
2022-11-11 12:21:20 +0545

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useRef, useEffect } from "react";
+import Form from "./components/Form";
+import ToggleButton from "./components/ToggleButton";
+import Todo from "./components/Todo";
+import { nanoid } from "nanoid";
 
-function App() {
+
+// const FILTER_MAP = {
+//   All: () => true,
+//   Active: task => !task.completed,
+//   Completed: task => task.completed
+// };
+
+// const FILTER_NAMES = Object.keys(FILTER_MAP);
+
+function App(props) {
+  const [tasks, setTasks] = useState(props.tasks);
+  // const [filter, setFilter] = useState('All');
+
+  function toggleTaskCompleted(id) {
+    const updatedTasks = tasks.map(task => {
+      if (id === task.id) {
+        return {...task, completed: !task.completed}
+      }
+      return task;
+    });
+    setTasks(updatedTasks);
+  }
+
+
+  function deleteTask(id) {
+    const remainingTasks = tasks.filter(task => id !== task.id);
+    setTasks(remainingTasks);
+  }
+
+
+  const taskList = tasks
+  // .filter(FILTER_MAP[filter])
+  .map(task => (
+    <Todo
+      id={task.id}
+      name={task.name}
+      completed={task.completed}
+      key={task.id}
+      toggleTaskCompleted={toggleTaskCompleted}
+      deleteTask={deleteTask}
+    />
+  ));
+
+  // const filterList = FILTER_NAMES.map(name => (
+  //   <ToggleButton
+  //     key={name}
+  //     name={name}
+  //     isPressed={name === filter}
+  //     setFilter={setFilter}
+  //   />
+  // ));
+
+  function addTask(name) {
+    const newTask = { id:nanoid(), name: name, completed: false };
+    console.log(newTask)
+    setTasks([...tasks, newTask]);
+  }
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Form addTask={addTask} />
+      {/* <div className="filters btn-group stack-exception">
+        {filterList}
+      </div> */}
+      <h2>
+        Tasks Remaining
+      </h2>
+      <ul
+        role="list"
+        className="todo-list stack-large stack-exception"
+      >
+        {taskList}
+      </ul>
     </div>
   );
 }
---
+++ src/components/Todo.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+
+const Todo = (props) =>{
+  return(
+    <div className="">
+      <div className="c-cb">
+          <input
+            id={props.id}
+            type="checkbox"
+            defaultChecked={props.completed}
+            onChange={() => props.toggleTaskCompleted(props.id)}
+          />
+          <label className="" htmlFor={props.id}>
+            {props.name}
+          </label>
+          <button
+            type="button"
+            className="btn btn__danger"
+            onClick={() => props.deleteTask(props.id)}
+          >
+            Delete <span className="visually-hidden">{props.name}</span>
+          </button>
+        </div>
+    </div>
+  );
+}
+
+export default Todo;
